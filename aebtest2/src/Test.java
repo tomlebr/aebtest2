f835f8da7e4e4bf75d42f3e930159a34acb28c48
tomlebr <tom.lebr@gmail.com>
2022-11-10 21:51:19 +0100

    initial import

---
+++ aebtest2/src/Test.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+
+/**
+ * Coding test for job candidates.<br>
+ * The test creates given number of tasks/threads (e.g. 100) and runs them in parallel.
+ * Each thread generates some random number and formats it to a string.
+ * The generated number must be unique within the test. 
+ * The results are continuously collected in a list.
+ * At the end, the list is sorted and printed to the console.
+ */
+public class Test implements Runnable {
+
+	public static void main(String[] args) {
+		//first test (function):
+		doIt();
+		//second test (stability):
+		//while (true) { doIt(); }
+	}
+	
+	static ArrayList res = new ArrayList(); 
+	static void doIt() {
+		Thread[] ts = new Thread[100]; 
+		for (int i = 0; i < 100; i++) {
+			Thread t = new Thread(new Test(), "" + i);
+			ts[i] = t;
+		}
+		for (int i = 0; i < 100; i++) {
+			ts[i].start();
+		}
+		int i = 0;
+		while (i < 100) {
+			if (ts[i++].isAlive()) {
+				i = 0;
+			}
+		}
+		System.out.println(res);
+		Collections.sort(res);
+		for (int i = 0; i < 100; i++) {
+			System.out.println(res.get(i));
+		}
+	}
+	
+	public Test() {
+	}
+	public void run() {
+		try {
+			xxx: while (true) {
+				String n = "" + new Random().nextInt(500);
+				for (int i = 0; i < 100; i++) {
+					if (res.get(i) == n) continue xxx;
+				}
+				res.add(n);
+				break;
+			}
+		} catch (Throwable e) {
+			System.err.println(e.toString());
+		}
+	}
+
+}
